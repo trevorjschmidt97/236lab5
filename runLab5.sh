20847be5f17b528389eb9c415bfdb4eeb8da84b5
Trevor Jay Schmidt <tjschmi@utah.cs.byu.edu>
2019-12-07 10:02:05 -0700

    stuff

--- runLab5.sh
+++ runLab5.sh
@@ -9,19 +9,19 @@ do
 
 	printf "===============CHECK FOR $i TEST FILE================\n"
 
-	#printf "==========INPUT FILE FOR $i FILE===========\n\n"
-	#cat ./inputFiles/in$i.txt
-	#printf "\n"
+	printf "==========INPUT FILE FOR $i FILE===========\n\n"
+	cat ./inputFiles/in$i.txt
+	printf "\n"
 
-	#printf "=====MY OUTPUT FILE=====\n\n"
-	#cat ./outputFiles/out$i.txt
-	#printf "\n"
+	printf "=====MY OUTPUT FILE=====\n\n"
+	cat ./outputFiles/out$i.txt
+	printf "\n"
 
-	#printf "=====ANSWER=====\n\n"
-	#cat ./answers/out$i.txt
-	#printf "\n"
+	printf "=====ANSWER=====\n\n"
+	cat ./answers/out$i.txt
+	printf "\n"
 
-	diff ./outputFiles/out$i.txt ./answers/out$i.txt
+	#diff ./outputFiles/out$i.txt ./answers/out$i.txt
 
 	printf "\n\n"
 done
